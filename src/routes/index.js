922fb93fbedf9f85c4b365e80d8236cf2915d47b
Vuong Truong <truongquocvuongnb@gmail.com>
2022-05-09 13:01:59 +0700

    update sources

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,6 +2,7 @@ import siteRouter from './site.router.js';
 import coursesRouter from './courses.router.js';
 import videoRouter from './videos.router.js';
 import userRouter from './user.router.js';
+import usersRouter from './user.router.js';
 
 function route(app) {
     app.use('/user', userRouter);
